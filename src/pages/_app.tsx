d71828a644186da2894a9b562143d33a7e2653e1
shonmacray <shonmacray@gmail.com>
2023-04-24 18:20:53 +0300

    initial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import SiteHeader from "@/components/Header";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SiteHeader>
+      <Component {...pageProps} />
+    </SiteHeader>
+  );
 }
